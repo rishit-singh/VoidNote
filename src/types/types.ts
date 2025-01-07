5dde71da1bb59a8521aa3f0c31af90e459b81831
demilade111 <64733187+demilade111@users.noreply.github.com>
2025-01-06 18:59:31 -0800

    fix: Correct formatting of TypeScript interfaces in types.ts for better readability

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,21 +1,21 @@
 export interface Audio {
-     blob: Blob | null;
-     url: string | null;
-   }
-   
-   export interface NoteModel {
-    id: string;
-    transcript: string;
-    diarizationResults: Array<{
-      speaker: string;
-      text: string;
-    }>;
-    createdAt: Date | null;
-    audio: Blob | null;
-    url: string | null;
-  }
-   
-   export interface DiarizationResult {
-     speaker: string;
-     text: string;
-   }
+  blob: Blob | null;
+  url: string | null;
+}
+
+export interface NoteModel {
+  id: string;
+  transcript: string;
+  diarizationResults: Array<{
+    speaker: string;
+    text: string;
+  }>;
+  createdAt: Date | null;
+  audio: Blob | null;
+  url: string | null;
+}
+
+export interface DiarizationResult {
+  speaker: string;
+  text: string;
+}
